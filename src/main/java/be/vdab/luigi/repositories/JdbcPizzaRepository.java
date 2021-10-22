8483f42181f3d7cea895d2425e188767caa80e13
Muschke <mussche.yannick@hotmail.be>
2021-10-22 15:06:05 +0200

    test op repository OK

---
+++ src/main/java/be/vdab/luigi/repositories/JdbcPizzaRepository.java
@@ -0,0 +1,117 @@
+package be.vdab.luigi.repositories;
+
+import be.vdab.luigi.domain.Pizza;
+import be.vdab.luigi.exceptions.PizzaNietGevondenException;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+/*In een repository staat 1 methode gelijke aa n1 sql transactie, als je er meerdere wilt gebruiken in 1 actie
+* moet je de methods oproepen in je services*/
+
+@Repository
+class JdbcPizzaRepository implements PizzaRepository{
+    private final JdbcTemplate template; //om uw bean (@repository) te injecteren, nu kan je gegevens selecteren en updaten
+    private final SimpleJdbcInsert insert; //Om records toe te kunnen voegen
+    /*Hieronder maken we een rowmapper, die mapt alle gegevens vh object in 1 rij op de juiste manier, zo kan je makkelijk lijsten maken*/
+    private final RowMapper<Pizza> pizzaMapper = (result, rowNum) -> new Pizza(result.getLong("id"), result.getString("naam"), result.getBigDecimal("prijs"), result.getBoolean("pikant"));
+    /*Voor elke soort rijweergave heb je aparte mapper nodig, deze is dus om de unieke prijzen weer te geven, rijen met enkel de kolom prijs*/
+    private final RowMapper<BigDecimal> prijsMapper = (result, rowNum) -> result.getBigDecimal("prijs");
+    JdbcPizzaRepository(JdbcTemplate template) {
+        this.template = template;
+        insert = new SimpleJdbcInsert(template).withTableName("pizzas").usingGeneratedKeyColumns("id");
+        /*voor SimpleJdbcInsert, je moet de tabel benoemen, en als een kolom met PK automatisch nummert, ook die benoemen */
+    }
+
+    @Override
+    /*Deze methode werkt niet zoder de SimpleJdbcInsert*/
+    public long create(Pizza pizza) {
+        return insert.executeAndReturnKey(Map.of("naam", pizza.getNaam(), "prijs", pizza.getPrijs(), "pikant", pizza.isPikant())).longValue();
+        /*executeAndReturnKey voegt een record toe. De parameter is een Map. Die bevat een entry per kolom van het toe te voegen record. De key is een kolom naam.
+        De value is de waarde die je invult in die kolom. De method maakt zelf een SQL insert statement en voert dit uit.
+        De method geeft de automatisch gegenereerde primary key waarde als een Number. Jij neemt daarvan de long waarde.*/
+    }
+
+    @Override
+    /*het opdaten van meerdere records*/
+    public void update(Pizza pizza) {
+        var sql = "update pizzas set naam=?, prijs=?, pikant=? where id = ?";
+        if(template.update(sql, pizza.getNaam(), pizza.getPrijs(), pizza.isPikant(), pizza.getId()) == 0){
+        /*Bij meerdere statements doe je hetzelfde en geef je de parameters chronologisch in*/
+        throw new PizzaNietGevondenException();
+        }
+    }
+
+    @Override
+    public void delete(long id) {
+        template.update("delete from pizzas where id = ?", id);
+        /*tweede parameter is de waarde van wat in "?" ingegeven word*/
+    }
+
+    @Override
+    /*Om dit efficient te doen moet je dus eerst je rowmapper maken*/
+    public List<Pizza> findAll() {
+        var sql = "select id, naam, prijs, pikant from pizzas order by id";
+        return template.query(sql, pizzaMapper);
+    }
+
+    @Override
+    /*hier gaan we hetzelfde doen als de list al, maar nu met parameters. Je geeft gewoon de where prijs =? als parameter en geeft
+    * de waardes dan in na de sql en mapper*/
+    public List<Pizza> findPrijsBetween(BigDecimal van, BigDecimal tot) {
+        var sql = "select id, naam, prijs, pikant from pizzas where prijs between ? and ? order by prijs";
+        return template.query(sql, pizzaMapper, van, tot);
+    }
+
+    @Override
+    /*Het lezen van 1 record, je gebruikt optional zodat je geen null waarde terugkrijgt, er komt iets of er komt niets, komt er niets, wil je exceptie krijgen die
+    * aangeeft dat hij de waarde niet kan  vinden*/
+    public Optional<Pizza> findById(long id) {
+        /*! ALLES in tryblock*/
+        try {
+            var sql = "select id, naam, prijs, pikant from pizzas where id = ? ";
+            return Optional.of(template.queryForObject(sql, pizzaMapper, id));
+        } catch (IncorrectResultSizeDataAccessException ex) {
+            return Optional.empty();
+        }
+    }
+
+
+    @Override
+    //Scalar Value --> is het resultaat van een select statement met 1 rij en 1 kolom.
+    public long findAantal() {
+        return template.queryForObject("select count(*) from pizzas", Long.class);
+        /*tweede parameteer is het type van je returnwaarde*/
+    }
+
+    @Override
+    /*aparte rowmapper nodig, deze zoekt gewoon lijst van de prijzen, eigenlijk echt domme opzoeking*/
+    public List<BigDecimal> findUniekePrijzen() {
+        return template.query("select distinct prijs from pizzas order by prijs", prijsMapper);
+    }
+
+    @Override
+    public List<Pizza> findByPrijs(BigDecimal prijs) {
+        var sql = "select id, naam, prijs, pikant from pizzas where prijs = ? order by naam";
+        return template.query(sql, pizzaMapper,prijs);
+    }
+
+    @Override
+    public List<Pizza> findByIds(Set<Long> ids) {
+        if(ids.isEmpty()) {
+            return List.of();
+        }
+        /*Als de verzameling id's leeg is geef je lege lijst terug, je doet geen opzoeking*/
+        var sql = "select id, naam, prijs, pikant from pizzas where id in (?,".repeat(ids.size()-1) + "?) order by id";
+        return template.query(sql, pizzaMapper, ids.toArray());
+        /*De logica hiervan is moeilijk te begrijpen en kan je best gewoon vanbuiten leren*/
+    }
+}
---
+++ src/test/java/be/vdab/luigi/repositories/JdbcPizzaRepositoryTest.java
@@ -0,0 +1,151 @@
+package be.vdab.luigi.repositories;
+
+
+import be.vdab.luigi.domain.Pizza;
+import be.vdab.luigi.exceptions.PizzaNietGevondenException;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+@JdbcTest
+@Import(JdbcPizzaRepository.class)
+@Sql("/insertPizzas.sql")
+class JdbcPizzaRepositoryTest extends AbstractTransactionalJUnit4SpringContextTests {
+
+    /*The above is syntax, remembah*/
+    private static final String PIZZAS = "pizzas";
+    private final JdbcPizzaRepository repository;
+
+    JdbcPizzaRepositoryTest(JdbcPizzaRepository repository) {
+        this.repository = repository;
+    }
+    /*Bij het uitvoeren van élke test doet spring:
+    *   - Transactie starten
+    *   - insertPizza.sql uitvoeren (=insert into pizzas(naam, prijs, pikant) values ('test', 10, false), ('test2', 20, true);)
+    *           !Dit word uitgevoerd bovenop de bestaande tabel in de test mode, dwz dat ook de originele records erin zitten
+    *   - De test uitvoeren in die transactie
+    *   - Rollback transactie (of ongedaan maken)
+    * Dit is heel belangrijk gegeven. We gaan 2 private functies maken voor een test op te doen, normaal plaats je die van onder.
+    * in de curus staan ze heel ongelukkig geplaatst en verwarrend, maar eigenlijk fungeren die functies meer als before each.
+    * want voor elke test worden test en test2 ingevoegd in de tabel, dus als je daarvan iets wil opvragen in je test, je kan dat
+    * best in private functie voorzien*/
+
+    private long idVanTestPizza() {
+        return jdbcTemplate.queryForObject("select id from pizzas where naam = 'test'", long.class);
+    }
+    private long idVanTest2Pizza() {
+        return jdbcTemplate.queryForObject("select id from pizzas where naam = 'test2'", long.class);
+    }
+
+    @Test
+    void findAantal() {
+        assertThat(repository.findAantal()).isEqualTo(countRowsInTable(PIZZAS));
+    }
+    @Test
+    void findAllGeeftAllePizzasGesorteerdOpId() {
+        assertThat(repository.findAll()).hasSize(countRowsInTable(PIZZAS)).extracting(Pizza::getId).isSorted();
+
+    }
+    /*Volgende test gaan in onze insertPizzas.Sql nakijken*/
+    @Test
+    void create() {
+        var id = repository.create(new Pizza(0, "test2", BigDecimal.TEN, false));
+        /*Als de autonummering van ons systeem klopt, moet de ingave van het id 0 falen, en moet hij automatische positief nummeren*/
+        assertThat(id).isPositive();
+        /*Dat wordt hier getest*/
+        assertThat(countRowsInTableWhere(PIZZAS, "id = " + id)).isOne();
+        /*Je resultaat van opzoeking van unieke gegeven moet kan altijd maar 1 rij zijn*/
+    }
+    @Test
+    void delete() {
+        var id = idVanTestPizza();
+        repository.delete(id);
+        /*cursus(deze zoekt op of je het verwijderde id nog kan vinden):*/
+        assertThat(countRowsInTableWhere(PIZZAS, "id = " + id)).isZero();
+    }
+    @Test
+    void findById() {
+        assertThat(repository.findById(idVanTestPizza()))
+                /*hasValueSatisfying --> Om lambda op optional toe te passen*/
+                .hasValueSatisfying(pizza -> assertThat(pizza.getNaam()).isEqualTo("test"));
+                /* Anders ga je nooit uit de net gevonden rij de naam kunnen halen om te controleren*/
+    }
+    @Test
+    void findByOnbestaandeIdVindtGeenPizza() {
+        assertThat(repository.findById(-1)).isEmpty();
+    }
+    @Test
+    void update() {
+        var id = idVanTestPizza();
+        /*de var pizza van de cursus is compleet nutteloos, de test slaagt op niets want je update de gegevens met crack dezelfde gegevens.
+        * Best dus om effectief iets te wijzigen */
+        var pizza = new Pizza(id, "test", BigDecimal.ONE, false);
+        repository.update(pizza);
+        assertThat(countRowsInTableWhere(PIZZAS, "prijs=1 and id=" + id)).isOne();
+    }
+    @Test
+    void updateOnbestaandePizzaGeeftEenFout() {
+        assertThatExceptionOfType(PizzaNietGevondenException.class).isThrownBy(
+                () -> repository.update(new Pizza(-1, "test", BigDecimal.TEN, false)));
+        /*Dit moet fout geven, want je probeert pizza met id -1 up te daten, want onmogelijk is*/
+    }
+    @Test
+    void findByPrijsBetween() {
+        var van = BigDecimal.ONE;
+        var tot = BigDecimal.TEN;
+        assertThat(repository.findPrijsBetween(van, tot))
+                .hasSize(super.countRowsInTableWhere(PIZZAS, "prijs between 1 and 10"))
+               /* .extracting(pizza -> pizza.getPrijs())  onderstaande is de lambda verkort */
+                .extracting(Pizza::getPrijs)
+                .allSatisfy(prijs -> assertThat(prijs).isBetween(van, tot));
+                /*allSatisfy -> test alle objecten in lijst aan parameter*/
+    }
+    @Test
+    void findUniekePrijzenGeeftPrijzenOplopend() {
+        assertThat(repository.findUniekePrijzen())
+                .hasSize(jdbcTemplate.queryForObject("select count(distinct prijs) from pizzas", Integer.class))
+                /*Controle 1: we tellen aantal unieke prijzen en kijken of dit aantal even groot is als wat methode
+                * findUniekePrijzen terugbrengt*/
+                .doesNotHaveDuplicates() //controle 2: distinct, zijn ze uniek?
+                .isSorted(); //controle 3: sortering
+    }
+    @Test
+    void findByPrijs() {
+        assertThat(repository.findByPrijs(BigDecimal.TEN))
+                .hasSize(super.countRowsInTableWhere(PIZZAS, "prijs = 10"))
+                /*hasSize super't iets van de class waaruit we extenden, ik weet ook niet wat*/
+                .allSatisfy(pizza -> assertThat(pizza.getPrijs()).isEqualByComparingTo(BigDecimal.TEN))
+                .extracting(Pizza::getNaam)
+                .isSortedAccordingTo(String::compareToIgnoreCase);
+                /*(String::compareToIgnoreCase) haalt gewoon de hoofdletters weg uit de sortering die je controleert
+                * anders geeft die fout want hoofdletters worden voor kleine lettters gesorteerd (A, B, C, a, b, c,..).
+                * SQL sorteert hoofdletter onafhankelijk*/
+    }
+    @Test
+    void findByIds() {
+        long id1 = idVanTestPizza();
+        long id2 = idVanTest2Pizza();
+        assertThat(repository.findByIds(Set.of(id1, id2)))
+                .extracting(Pizza::getId)
+                .containsOnly(id1, id2)
+                .isSorted();
+    }
+    @Test
+    void findByIdsGeeftLegeVerzamelingPizzasBijLegeVerzamelingIds() {
+        assertThat(repository.findByIds(Set.of())).isEmpty();
+    }
+    @Test
+    void findByIdsGeeftLegeVerzamelingPizzasBijOnbestaandeIds() {
+        assertThat(repository.findByIds(Set.of(-1L, -2L))).isEmpty();
+    }
+
+}
